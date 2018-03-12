ca91eb1fdf6a71c01c3ac60595b1192527ec1f45
Ian Harwell <sirharwell@gmail.com>
2018-03-12 11:25:09 -0600

    navbar and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import NoMatch from './components/NoMatch';
+import NavBar from './components/NavBar';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <NavBar  />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route component={NoMatch} />
+    </Switch>
+  </div>
+)
+
+
+
+
+
+
+
+
+
 
 export default App;
---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NoMatch = () => (
+  <h5> 404 </h5>
+)
+
+export default NoMatch;
